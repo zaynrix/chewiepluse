eb50582d9dea2c9cb6b77a45a58d21f87a9fd137
mac <zayns1914@gmail.com>
2022-10-23 10:30:10 +0300

    add components

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:audio_learning/video_items.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
-void main() => runApp(Home());
+void main() => runApp(const Home());
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -16,191 +16,69 @@ class Home extends StatelessWidget {
 }
 
 class HomeScreen extends StatelessWidget {
+  final List<String> links = [
+    'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1204-large.mp4',
+    'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1205-large.mp4',
+    'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1206-large.mp4',
+    'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1207-large.mp4',
+    'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1208-large.mp4',
+    'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1209-large.mp4',
+    'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1210-large.mp4'
+  ];
+
+  HomeScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey[100],
-      // appBar: AppBar(
-      //   automaticallyImplyLeading: false,
-      //   title: Text('Flutter Video Player Demo'),
-      //   centerTitle: true,
-      // ),
-      body: SafeArea(
-        child: ListView(
-          children: <Widget>[
-            VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1204-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),  VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1205-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),  VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1206-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),  VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1207-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),  VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1208-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),  VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1209-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),  VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1210-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),  VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1211-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),  VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1212-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),  VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1213-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),  VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1214-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),  VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1215-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),  VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1216-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ), VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1217-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ), VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1218-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ), VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1219-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),   VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1220-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),   VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1221-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),   VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1222-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),   VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1223-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1224-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1225-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ), VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1226-large.mp4'),
-              looping: false,
+      extendBodyBehindAppBar: true,
+
+      backgroundColor: Colors.transparent,
+      // appBar:
+      // floatingActionButton:
+      body: Stack(
+        children: [
+          PageView.builder(
+            itemCount: links.length,
+            itemBuilder: (context, index) => VideoItems(
+              videoPlayerController:
+                  VideoPlayerController.network(links[index]),
+              looping: true,
               autoplay: true,
             ),
-            VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1227-large.mp4'),
-              looping: false,
-              autoplay: true,
+            scrollDirection: Axis.vertical,
+          ),
+          Positioned(
+            top: 0.0,
+            left: 0.0,
+            right: 0.0,
+            child: AppBar(
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+              title:  Center(
+                child: Row(
+
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    TextButton(
+                      child:const Text(
+                        "Following",
+                        style: TextStyle(color: Colors.white,fontSize: 20),
+                      ),
+                      onPressed: (){},
+                    ), TextButton(
+                      child:const Text(
+                        "For You",
+                        style: TextStyle(color: Colors.white,fontSize: 20),
+                      ),
+                      onPressed: (){},
+                    ),
+                  ],
+                ),
+              ),
             ),
-            VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1228-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),
-            VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1229-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),
-            VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1230-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),
-            VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1231-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),  VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1232-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),  VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1233-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),  VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1234-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),  VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1235-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),  VideoItems(
-              videoPlayerController: VideoPlayerController.network(
-                  'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1236-large.mp4'),
-              looping: false,
-              autoplay: true,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/video_items.dart
+++ lib/video_items.dart
@@ -3,33 +3,56 @@ import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
 class VideoItems extends StatefulWidget {
-   final  VideoPlayerController videoPlayerController;
+  final VideoPlayerController videoPlayerController;
   final bool? looping;
   final bool? autoplay;
 
-
-  const VideoItems({Key? key,
+  const VideoItems({
+    Key? key,
     required this.videoPlayerController,
-    this.looping, this.autoplay,
-  }) : super(key: key) ;
+    this.looping,
+    this.autoplay,
+  }) : super(key: key);
 
   @override
   _VideoItemsState createState() => _VideoItemsState();
 }
 
 class _VideoItemsState extends State<VideoItems> {
- late ChewieController _chewieController;
+  // Chewie Controller
+  late ChewieController _chewieController;
 
   @override
   void initState() {
     super.initState();
+
+    // Initialized Controller and Arguments
     _chewieController = ChewieController(
       allowFullScreen: true,
+
+      // Video : assets-network-files
       videoPlayerController: widget.videoPlayerController,
-      // aspectRatio:5/8,
+      showControls: false,
+      isLive: true,
+
+      cupertinoProgressColors: ChewieProgressColors(
+          playedColor: Colors.red,
+          backgroundColor: Colors.red,
+          bufferedColor: Colors.red,
+          handleColor: Colors.red),
+      materialProgressColors: ChewieProgressColors(
+          playedColor: Colors.red,
+          backgroundColor: Colors.red,
+          bufferedColor: Colors.red,
+          handleColor: Colors.red),
       autoInitialize: true,
       autoPlay: widget.autoplay!,
       looping: widget.looping!,
+      // Error Builder
+      placeholder: const Center(
+          child: CircularProgressIndicator(
+        color: Colors.red,
+      )),
       errorBuilder: (context, errorMessage) {
         return Center(
           child: Text(
@@ -41,6 +64,7 @@ class _VideoItemsState extends State<VideoItems> {
     );
   }
 
+  // Dispose Controller
   @override
   void dispose() {
     super.dispose();
@@ -49,13 +73,109 @@ class _VideoItemsState extends State<VideoItems> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 500,
-      child: Chewie(
-
-        controller: _chewieController,
-      ),
+    return Stack(
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Chewie(
+            controller: _chewieController,
+          ),
+        ),
+        Positioned(
+          bottom: 10,
+          right: 10,
+          // alignment: Alignment.bottomRight,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.favorite,
+                    color: Colors.white,
+                    size: 36,
+                  )),
+              const Text(
+                "85.1K",
+                style: TextStyle(color: Colors.white),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.comment,
+                    color: Colors.white,
+                    size: 36,
+                  )),
+              const Text(
+                "85.1K",
+                style: TextStyle(color: Colors.white),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.assistant_photo_rounded,
+                    color: Colors.white,
+                    size: 36,
+                  )),
+              const Text(
+                "201",
+                style: TextStyle(color: Colors.white),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.reply_outlined,
+                    color: Colors.white,
+                    size: 36,
+                  )),
+              const Text(
+                "201",
+                style: TextStyle(color: Colors.white),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          bottom: 30,
+          left: 10,
+          // alignment: Alignment.bottomRight,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: const [
+              Text(
+                "@Username",
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Lorem ipesum Text style Desciptios",
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
-
 }
